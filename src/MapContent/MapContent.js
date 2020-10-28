235c3822eb04be83e21882697d54b8313168fe21
Byungchan-Park <pbc9236@gmail.com>
2020-10-29 00:38:21 +0900

    feat: Make and put markers of current position and nearby bus stops  on the map

--- src/MapContent/MapContent.js
+++ src/MapContent/MapContent.js
@@ -12,6 +12,33 @@ const loadMap = (currentPos, nearbyBusStopInfo) => {
 
   // 지도 생성
   const map = new kakao.maps.Map(mapContainer, mapOptions)
+
+  // 마커들의 아이콘 생성
+  const currentPosIcon = new kakao.maps.MarkerImage("https://i1.daumcdn.net/dmaps/apis/n_local_blit_04.png", new kakao.maps.Size(31, 35))
+  const busStopIcon = new kakao.maps.MarkerImage("https://icon-icons.com/icons2/567/PNG/32/bus_icon-icons.com_54412.png", new kakao.maps.Size(25, 25))
+
+  // 현재 위치를 표시해주는 마커 생성
+  const currentPosMarker = new kakao.maps.Marker({
+    position: new kakao.maps.LatLng(currentPos.lat, currentPos.lng),
+    image: currentPosIcon,
+  })
+  currentPosMarker.setMap(map)
+
+  // 주변 버스 정류장을 표시해주는 마커 생성
+  nearbyBusStopInfo.forEach((el) => {
+    // 마커 생성
+    const marker = new kakao.maps.Marker({
+      //마커가 표시 될 위치
+      position: new kakao.maps.LatLng(el.gpslati, el.gpslong),
+      //마커에 hover시 나타날 title
+      title: el.title,
+      image: busStopIcon,
+      clickable: true, // 지도 클릭 이벤트 발생 방지
+    })
+
+    // 마커를 지도에 표시
+    marker.setMap(map)
+  })
 }
 
 function MapContent({ currentPos, nearbyBusStopInfo }) {
